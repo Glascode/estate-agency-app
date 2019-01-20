2357dc86d54bbff45a3626e0e3e059d4b18e7a8c
Romain Garcia <diabl0x61@gmail.com>
2019-01-20 14:36:58 +0100

    Creation of the basic activities;

---
+++ app/src/main/java/io/github/glascode/estateagency/PropertyActivity.java
@@ -0,0 +1,15 @@
+package io.github.glascode.estateagency;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PropertyActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_property);
+	}
+
+	//Todo: Create the methods linking the variables with the actual values
+}
