98f2bb6c524ad208ee5d883014208b237198ed6d
Glascode <mael.querre@gmail.com>
2019-01-31 15:08:56 +0100

    Initialise Http request

--- app/src/main/java/io/github/glascode/estateagency/MainActivity.java
+++ app/src/main/java/io/github/glascode/estateagency/MainActivity.java
@@ -1,13 +1,81 @@
 package io.github.glascode.estateagency;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	public void okhttp(View view) {
+		makeHttpRequest("https://ensweb.users.info.unicaen.fr/android-estate/mock-api/immobilier.json");
+	}
+
+	private void makeHttpRequest(String url) {
+		OkHttpClient client = new OkHttpClient();
+
+		Request request = new Request.Builder().url(url).build();
+
+		client.newCall(request).enqueue(new Callback() {
+			@Override
+			public void onFailure(Call call, IOException e) {
+				e.printStackTrace();
+			}
+
+			@Override
+			public void onResponse(Call call, Response response) throws IOException {
+				try (ResponseBody responseBody = response.body()) {
+					if (!response.isSuccessful()) {
+						throw new IOException("Unexpected HTTP code " + response);
+					}
+
+					Headers responseHeaders = response.headers();
+					for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+						Log.i("JML", responseHeaders.name(i) + ": " + responseHeaders.value(i));
+					}
+
+					Snackbar.make(findViewById(R.id.mainLayout), responseBody.string(), Snackbar.LENGTH_LONG).show();
+
+					//Property property1 = makePropertyFromJson(responseBody.string());
+				}
+			}
+		});
+	}
+
+	public Property makePropertyFromJson(String jsonString) {
+		Moshi moshi = new Moshi.Builder().build();
+		JsonAdapter<Property> jsonAdapter = moshi.adapter(Property.class);
+
+		try {
+			Property property = jsonAdapter.fromJson(jsonString);
+			String message = property.getId() + ", " + property.getTitle() + ", " + property.getDescription();
+			Snackbar.make( findViewById(R.id.mainLayout), message, Snackbar.LENGTH_LONG).show();
+			return property;
+		} catch (IOException e) {
+			Log.i("JML", "Erreur I/O");
+		}
+
+		return null;
+	}
+
 }
--- app/src/main/java/io/github/glascode/estateagency/Property.java
+++ app/src/main/java/io/github/glascode/estateagency/Property.java
@@ -5,16 +5,109 @@ import java.util.List;
 
 public class Property {
 
-    private int id;
-    private String title;
-    private String description;
-    private int rooms;
-    private List<String> features;
-    private int price;
-    private String city;
-    private int zipCode;
-    private Seller seller;
-    private List<String> imagesUrls;
-    private Date date;
+	private int id;
+	private String title;
+	private String description;
+	private int rooms;
+	private List<String> features;
+	private int price;
+	private String city;
+	private int zipCode;
+	private Seller seller;
+	private List<String> imagesUrls;
+	private Date date;
 
+	public Property(int id, String title, String description) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getRooms() {
+		return rooms;
+	}
+
+	public void setRooms(int rooms) {
+		this.rooms = rooms;
+	}
+
+	public List<String> getFeatures() {
+		return features;
+	}
+
+	public void addFeature(String feature) {
+		features.add(feature);
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public Seller getSeller() {
+		return seller;
+	}
+
+	public void setSeller(Seller seller) {
+		this.seller = seller;
+	}
+
+	public List<String> getImagesUrls() {
+		return imagesUrls;
+	}
+
+	public void addImageUrl(String imageUrl) {
+		imagesUrls.add(imageUrl);
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
 }
